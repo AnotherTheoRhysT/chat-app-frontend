597f1e21990ec15553825bd5d38bd26a254324ad
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-11 20:48:30 +0800

    Chat App global styling

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,8 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+export default function Document({ __NEXT_DATA__ }) {
+  const getStyles = () => {
+    const {page} = __NEXT_DATA__
+    if (page.split("/")[1] === "chat") {
+      return (
+        <style jsx global>{`
+          html, body {
+            height: 100dvh;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            background-color: red;
+          }
+        `}</style>
+        )
+    } else {
+      return ''
+    }
+  }
+
   return (
     <Html lang="en">
+      {/* {getStyles()} */}
       <Head />
       <body>
         <Main />
