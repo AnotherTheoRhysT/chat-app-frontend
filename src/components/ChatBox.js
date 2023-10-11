7570d4eb4ede8c7aa50f2240e495fc30935e6183
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-11 20:49:39 +0800

    Send message using Shadcdn

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,12 +1,60 @@
-import { AuthContext } from '@/pages/_app'
-import React, { useContext } from 'react'
+import React from 'react'
+
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import {
+	Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+
+
+const FormSchema = z.object({
+  message: z.string()
+}).required()
 
 const ChatBox = () => {
-	const [auth, setAuth] = useContext(AuthContext)
+	const form = useForm({
+    resolver: zodResolver(FormSchema),
+  })
+
+	const onSubmit = (formData) => {
+		console.log(formData.message)
+		formData.message = ""
+	}
 
 	return (
-		auth && 
-		<div>ChatBox</div>
+		<div className='relative bottom-0 right-0 w-full p-2 border-t-slate-200 border-t-2 border-solid'>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 flex">
+					<FormField
+						control={form.control}
+						name="message"
+						render={({ field }) => (
+							<FormItem className='flex-1'>
+								<FormControl>
+									<Textarea
+										placeholder="Aa"
+										className="resize-none flex-1"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">Submit</Button>
+				</form>
+			</Form>
+
+		</div>
 	)
 }
 
