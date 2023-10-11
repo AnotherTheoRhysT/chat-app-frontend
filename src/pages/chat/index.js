6389238693176a304450e2df3ef03e25ca1b6dcd
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-11 20:46:18 +0800

    ChatApp Authorization

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,12 +1,14 @@
 import ChatLayout from '@/components/ChatLayout'
-import React from 'react'
-import ChatBox from '@/components/ChatBox'
+import React, { useContext } from 'react'
+import { AuthContext } from '../_app'
+import ChatApp from '@/components/ChatApp'
 
 const Page = () => {
+	const [auth, setAuth] = useContext(AuthContext)
 	return (
 		<ChatLayout>
 			<div>Chat</div>
-			<ChatBox />
+			{ auth && <ChatApp /> }
 		</ChatLayout>
 	)
 }
